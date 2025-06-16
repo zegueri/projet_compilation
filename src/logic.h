6104dfad7946535fa3fc32522a16b1e9821099cb
Jounaïd Boudefar <119979021+zegueri@users.noreply.github.com>
2025-06-16 14:27:01 +0200

    Add formula storage and command

--- src/logic.h
+++ src/logic.h
@@ -11,11 +11,13 @@ typedef struct {
     char vars[MAX_VARS][MAX_NAME]; /* variable names */
     int num_entries;               /* size of table = 1<<arity */
     unsigned char table[1<<MAX_VARS];
+    char *formula;                 /* optional textual representation */
 } Function;
 
 void logic_init(void);
 int add_function_table(const char *name, int arity, const char vars[][MAX_NAME],
-                       const unsigned char *table, int num_entries);
+                       const unsigned char *table, int num_entries,
+                       const char *formula);
 void list_functions(void);
 
 void print_varlist(const char *name);
@@ -23,5 +25,7 @@ void print_varlist(const char *name);
 void print_table(const char *name);
 
 void eval_and_print(const char *name, const int *values, int value_count);
+
+void print_formula(const char *name);
 
 #endif
